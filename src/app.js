f1bb7f5441cec7683f866b603bef1a61ae877c1b
Gabriel <gabrielncosta208@gmail.com>
2020-12-05 11:00:25 -0300

    feat(repositories): post repository route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,11 +11,17 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json({ repositories });
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const newRepository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(newRepository)
+
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
